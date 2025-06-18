17934f40aa93e7f82124ed03641e0389787b30b1
Gen <e89030922661@gmail.com>
2025-06-18 19:25:20 +0300

    build: final clean-ups for vercel

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,5 @@
 
-import { config } from 'dotenv'; config({ path: '../.env' }); // ⬅️ перед defineConfig
-import { defineConfig } from 'drizzle-kit';
+import 'dotenv/config';
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
@@ -8,7 +7,7 @@ if (!process.env.DATABASE_URL) {
 
 export default defineConfig({
   out: "./migrations",
-  schema: "./schema/schema.ts", // ✅ актуальный путь
+  schema: "./schema.ts", // ✅ актуальный путь
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
